8e4987d9a1c4587b5353ca9e29407cbd7626b4f4
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-18 23:20:21 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +11,60 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+   song = self.new
+   song.save
+   song
+ end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if @@all.include?(song_name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+
+  def self.alphabetical
+    @@all.uniq.sort_by {|song| song.name }
+  end
+
+  def self.new_from_filename(file)
+    song = Song.new
+    song_array = file.split(" - ")
+    song_name = song_array[1].chomp(".mp3")
+    song.name = song_name
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
